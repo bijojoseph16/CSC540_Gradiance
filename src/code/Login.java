6bddaf076f82dc51672260de6b320382fa599a8b
pgupta9 <pratyush.g1994@gmail.com>
2017-10-27 16:23:57 -0400

    first commit

--- src/code/Login.java
+++ src/code/Login.java
@@ -1,5 +1,6 @@
 package code;
 import java.sql.*;
+
 import java.util.*;
 import dbconnect.Connect;
 
@@ -7,10 +8,24 @@ public class Login {
 
     // Method to display the initial menu options to the user.
     public static void startPage(Scanner ip) throws NoSuchElementException{
+    	
         System.out.println("\n*****Gradience*****");
             System.out.println("1. Admin/Student Login");
             System.out.println("2. Exit");
             System.out.print("Choice : ");
-        
+            try {
+            PreparedStatement pstmt=Connect.getConnection().prepareStatement(Queries.getstudents);
+            ResultSet rs=pstmt.executeQuery();
+            
+            while(rs.next()) {
+            	
+            	System.out.println(rs.getString(2));
+            	
+            	
+            }
+            }
+            catch(SQLException e) {
+            	e.printStackTrace();
+            }
     }
 }
